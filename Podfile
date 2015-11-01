8567a70ed4bb5c3315ea700226cc40f71f09ac5b
Marco Lau <marco.lau@industrieit.com>
2015-11-01 16:06:30 +0800

    Add CocoaPods

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'YWeatherPrototype' do
+platform :ios, '7.0'
+pod 'ECSlidingViewController', '~> 2.0.3'
+end
+
+target 'YWeatherPrototypeTests' do
+
+end
+
+target 'YWeatherPrototypeUITests' do
+
+end
+
---
+++ YWeatherPrototype/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YWeatherPrototype
+//
+//  Created by Marco Lau on 1/11/2015.
+//  Copyright © 2015 Marco Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
